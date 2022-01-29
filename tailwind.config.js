d28ea67a7792beb9012ca57abee04045763a8c06
sidd-oo <sahoosiddharth3@gmail.com>
2022-01-29 22:23:01 +0530

    feat: Configured tailwind with some new colors and fill

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,28 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {}
+    extend: {},
+    fill: (theme) => ({
+      red: theme('colors.red.primary')
+    }),
+    colors: {
+      white: '#ffffff',
+      blue: {
+        medium: '#005c98'
+      },
+      black: {
+        light: '#262626',
+        faded: '#00000059'
+      },
+      gray: {
+        base: '#616161',
+        background: '#fafafa',
+        primary: '#dbdbdb'
+      },
+      red: {
+        primary: '#ed4956'
+      }
+    }
   },
   plugins: []
 };
